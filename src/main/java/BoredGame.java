fe3f759c0f4f849c7c4142c07275e0497ced7e08
yunfei zong <yun@tests-MacBook-Pro.local>
2014-11-08 16:51:42 -0800

    quad

--- src/main/java/BoredGame.java
+++ src/main/java/BoredGame.java
@@ -1,23 +1,53 @@
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 /**
  * Created by yun on 11/8/14.
  */
 public class BoredGame {
 
-    public BoredGame(){
+    public void start() {
         try {
-            System.out.println("i hope");
-            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.setDisplayMode(new DisplayMode(800,600));
+            Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
+            System.exit(0);
         }
+
+        // init OpenGL
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, 800, 0, 600, 1, -1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+
+        while (!Display.isCloseRequested()) {
+            // Clear the screen and depth buffer
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+            // set the color of the quad (R,G,B,A)
+            GL11.glColor3f(0.5f,0.5f,1.0f);
+
+            // draw quad
+            GL11.glBegin(GL11.GL_QUADS);
+            GL11.glVertex2f(100,100);
+            GL11.glVertex2f(100+200,100);
+            GL11.glVertex2f(100+200,100+200);
+            GL11.glVertex2f(100,100+200);
+            GL11.glEnd();
+
+            Display.update();
+        }
+
+        Display.destroy();
     }
 
-    public static void main(String[] args){
-        new BoredGame();
+
+    public static void main(String[] argv) {
+        BoredGame displayExample = new BoredGame();
+        displayExample.start();
     }
 }
 
